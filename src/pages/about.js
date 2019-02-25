30432189493a3959a848cf6a873bc11ad0fb1619
mk-hill <40850298+mk-hill@users.noreply.github.com>
2019-02-25 11:20:22 -0600

    Add eslint, about page

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../components/layout';
+
+const About = () => (
+  <Layout>
+    <h1>About Us</h1>
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse iusto maiores dolore tempora
+    aliquid necessitatibus vero nisi autem error expedita officiis ipsa eligendi sapiente excepturi,
+    ad accusamus beatae quam? Esse.
+  </Layout>
+);
+
+export default About;
